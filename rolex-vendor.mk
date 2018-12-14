5364de42bf5e03151c0d686506b61092730f136b
AlexDeltax <alexsmolenskyi@gmail.com>
2018-12-15 01:40:52 +0200

    added KCAL app

--- rolex-vendor.mk
+++ rolex-vendor.mk
@@ -2458,6 +2458,7 @@ PRODUCT_PACKAGES += \
     com.qualcomm.location \
     qcrilmsgtunnel \
     TimeService \
+    ColorControl \
     qdcm_calib_data_hx8394F_HD720p_video_BOE_c3a \
     qdcm_calib_data_ili9881c_HD720p_video_Ebbg_c3a \
     qdcm_calib_data_nt35521s_HD720p_video_EBBG_c3a
